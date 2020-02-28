ced9dec4d0abdf923e7632ef470fb2883fc82017
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2020-02-28 18:56:27 +0100

    Update

--- code.js
+++ code.js
@@ -1,12 +1,13 @@
 figma.showUI(__html__);
-
 figma.ui.resize(350, 170);
 
 figma.ui.onmessage = message => {
+  // Fired if user wants to resize the plugin window
   if (message.type === "resize") {
-    figma.ui.resize(message.width, message.height);
+    figma.ui.resize(message.message.width, message.message.height);
   }
 
+  // This is fired when user gets notified of something
   if (message.type === "notify") {
     figma.notify(message.message);
   }
